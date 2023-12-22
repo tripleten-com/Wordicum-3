6b0b23d2b8d8069b62eb5bb9dd6ad3d4193b9ef4
Anton Martynenko <120686503+Anton-Martynenko-TripleTen@users.noreply.github.com>
2023-12-22 14:28:03 +0000

    Change yatube -> wordicum

--- wordicum/wordicum/asgi.py
+++ wordicum/wordicum/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ya_tube project.
+ASGI config for wordicum project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ya_tube.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wordicum.settings')
 
 application = get_asgi_application()
--- wordicum/wordicum/wsgi.py
+++ wordicum/wordicum/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ya_tube project.
+WSGI config for wordicum project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ya_tube.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wordicum.settings')
 
 application = get_wsgi_application()
